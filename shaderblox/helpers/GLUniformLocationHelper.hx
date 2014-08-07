b1e6940bbbd6337783592891028051ab775ba72c
haxiomic <haxiomic@gmail.com>
2014-08-07 23:53:40 +0100

    Minor formatting

--- shaderblox/helpers/GLUniformLocationHelper.hx
+++ shaderblox/helpers/GLUniformLocationHelper.hx
@@ -3,12 +3,11 @@ package shaderblox.helpers;
 import lime.graphics.opengl.GLUniformLocation;
 
 class GLUniformLocationHelper{
-	static public function isValid(u:GLUniformLocation){
-		return
-			#if !js 
-			(u >= 0);
-			#else
-			(u != null);
-			#end
+	static public inline function isValid(u:GLUniformLocation){
+		return	#if !js 
+				(u >= 0);
+				#else
+				(u != null);
+				#end
 	}
 }
