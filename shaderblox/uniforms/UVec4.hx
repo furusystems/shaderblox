c9b6a943325e9c41cd055a1b8f297ebb1e1d26c3
haxiomic <haxiomic@gmail.com>
2014-08-11 16:51:25 +0100

    Using Vector4 instead of Vector3D

--- shaderblox/uniforms/UVec4.hx
+++ shaderblox/uniforms/UVec4.hx
@@ -4,7 +4,7 @@ import snow.render.opengl.GL;
 import falconer.utils.Vector3D;
 #elseif lime
 import lime.graphics.opengl.GL;
-import lime.utils.Vector3D;
+import lime.math.Vector4;
 import lime.graphics.opengl.GLUniformLocation;
 
 using shaderblox.helpers.GLUniformLocationHelper;
@@ -14,9 +14,9 @@ using shaderblox.helpers.GLUniformLocationHelper;
  * Vector4 float uniform
  * @author Andreas Rønning
  */
-class UVec4 extends UniformBase<Vector3D> implements IAppliable  {
+class UVec4 extends UniformBase<Vector4> implements IAppliable  {
 	public function new(name:String, index:GLUniformLocation, x:Float = 0, y:Float = 0, z:Float = 0, w:Float = 0) {
-		super(name, index, new Vector3D(x, y, z, w));
+		super(name, index, new Vector4(x, y, z, w));
 	}
 	public inline function apply():Void {
 		if (location.isValid()) {
