3153e03b5c2fb5e2f095a2d20edcc71bba0fd521
haxiomic <haxiomic@gmail.com>
2014-08-11 16:52:16 +0100

    Again with the minor formatting

--- shaderblox/helpers/GLUniformLocationHelper.hx
+++ shaderblox/helpers/GLUniformLocationHelper.hx
@@ -4,10 +4,6 @@ import lime.graphics.opengl.GLUniformLocation;
 
 class GLUniformLocationHelper{
 	static public inline function isValid(u:GLUniformLocation){
-		return	#if !js 
-				(u >= 0);
-				#else
-				(u != null);
-				#end
+		return  #if !js (u >= 0); #else (u != null); #end
 	}
 }
